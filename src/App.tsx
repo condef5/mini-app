c5c73c274387e2b3f008414c825c2ff945a9172b
Frank Condezo <frankcondezo@gmail.com>
2024-06-29 03:06:51 -0500

    test things

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import twaLogo from "./assets/tapps.png";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import WebApp from '@twa-dev/sdk'
+import WebApp from "@twa-dev/sdk";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function buyStars(amount: number) {
+    WebApp.showConfirm(
+      `Do you want to purchase ${amount} stars?`,
+      (confirmed) => {
+        if (confirmed) {
+          // User confirmed the purchase
+          WebApp.sendData(
+            JSON.stringify({
+              action: "buy_stars",
+              amount: amount,
+            })
+          );
+        } else {
+          // User canceled the purchase
+          console.log("Purchase canceled");
+        }
+      }
+    );
+  }
 
   return (
     <>
@@ -30,12 +50,17 @@ function App() {
       </div>
       {/*  */}
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
+        <button
+          onClick={() =>
+            WebApp.showAlert(`Hello World! Current count is ${count}`)
+          }
+        >
+          Show Alert
         </button>
+        <button onClick={() => buyStars(3)}>Show Payments</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
